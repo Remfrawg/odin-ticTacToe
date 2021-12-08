564ea151a8a353f582a95920edb5e29ef7750770
Remy Nguyen <Remytn97@gmail.com>
2021-12-08 14:56:17 -0800

    working game, needs CSS

--- script.js
+++ script.js
@@ -1,50 +1,127 @@
-var gameBoard = (function (){
-    let squares = [];
-    let container = document.querySelector("#gameContainer");
-        for (let i = 0; i<9; i++){
-            let newDiv = document.createElement('div');
-            newDiv.classList.add("gameSquare");
-            squares.push(newDiv);
-        };
+var player = (function (name, playerVal) {
+    let moves = [];
 
-    function render(){
-        squares.forEach(element =>{
-            container.appendChild(element);
-            element.addEventListener('click', () => gameLogic.addNewPlay(squares.indexOf(element)))
-        });
-    };
-    function clear(){
-        while (container.lastChild){
-            container.removeChild(container.lastChild);
-        }
+    function getMoves(){
+        return moves; 
     }
-
-    function addPlay(i, char){
-        squares[i].innerText = char;
-        //clear();
-        //render();
-
+    function addNewMove(newMove){
+        moves.push(newMove);
+    }
+    function reset (){
+        moves = [];
     }
     return {
-        render : render,
-        addPlay : addPlay,
+        name,
+        playerVal,
+        getMoves : getMoves,
+        addNewMove : addNewMove,
+        reset,
+    }
+});
 
+let player1 = player('', 'X');
+let player2 = player('', 'O');
+
+var gameBoard = (function (){
+    let squares = [];    
+    let container = document.querySelector("#gameContainer");
+    let playerTurn = document.querySelector("#playerTurn");
+    let startButton = document.querySelector("#submitStart");
+    let startForm = document.querySelector('#bgModal');
+    let endForm = document.querySelector('#bgCloseForm');
+    let resetButton = document.querySelector('#resetButton');
+    let newPlayers = document.querySelector('#newPlayers');
+    let exitButton = document.querySelector('#exit');
+
+    for (let i = 0; i<9; i++){
+        let newDiv = document.createElement('div');
+        newDiv.classList.add("gameSquare");
+        squares.push(newDiv);
+    };
+    squares.forEach(element =>{
+        container.appendChild(element);
+        element.addEventListener('click', () => gameLogic.addNewPlay(squares.indexOf(element)));
+    });
+
+    startButton.addEventListener('click', () =>{
+        startForm.style.display = 'none';
+        const _player1name = document.querySelector('#player1name');
+        const _player2name = document.querySelector('#player2name');
+        if (_player2name.value == ''){
+            player2.name = 'O';
+        }
+        else{
+            player2.name = _player2name.value;
+        }
+        if (_player1name.value == ''){
+            player1.name = 'X';
+        }
+        else{
+            player1.name = _player1name.value;
+        }
+        changeTurnCard(player1.name);
+    });
+    resetButton.addEventListener('click', ()=>{
+        _reset();
+        changeTurnCard(player1.name);
+        endForm.style.display = 'none';
+    });
+    newPlayers.addEventListener('click', ()=>{
+        _reset();
+        startForm.style.display = 'flex';
+        endForm.style.display = 'none';
+    });
+    exitButton.addEventListener('click', ()=>{
+        endForm.style.display = 'none';
+        changeTurnCard('',true);
+    });
+
+    function _reset(){
+        for (let i = 0; i<9; i++){
+            squares[i].innerText = '';
+        }
+        gameLogic.reset();
+    }
+    function addPlay(i, char){
+        squares[i].innerText = char;
+    }
+    function changeTurnCard(player, quit = false){
+        if (quit == false){
+        playerTurn.textContent = ` ${(player.toUpperCase())} GO! SEEK! DESTROY!`
+        }
+        else {
+            playerTurn.textContent = `Game Over`;
+        }
+    }
+    function closeOptions(){
+        endForm.style.display = 'flex';
+    }
+    return {
+        addPlay,
+        changeTurnCard,
+        closeOptions,
     };
 })();
+
 
 var gameLogic = (function (){
     const wins = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
     let counter = 0;
+    let playing = true;
+    let _nextPlayer = player2;
     function addNewPlay(index){
-         if (!(player1.getMoves().includes(index) || player2.getMoves().includes(index))){
-            let currentPlayer = _whoseTurn();
-             currentPlayer.addNewMove(index);
-             gameBoard.addPlay(index, currentPlayer.playerVal);
-             _checkWin(currentPlayer, index);
-             
-        }
-        
-    }
+         if (!(player1.getMoves().includes(index) || player2.getMoves().includes(index) || playing == false)){
+            let _currentPlayer = _whoseTurn();
+             _currentPlayer.addNewMove(index);
+             gameBoard.addPlay(index, _currentPlayer.playerVal);
+             _checkWin(_currentPlayer, index);
+             if ((player1.getMoves().length + player2.getMoves().length) >=9 && playing == true){
+                 alert("tie!");
+             };
+             gameBoard.changeTurnCard(_nextPlayer.name);
+             _nextPlayer = _currentPlayer;
+        };  
+    };
     function _whoseTurn (){
         if (counter %2 == 0){
             counter+=1;
@@ -53,53 +130,31 @@ var gameLogic = (function (){
         else{
             counter+=1;
             return player2;
-        }
-    }
-    function _checkWin(currentPlayer, index){
+        };
+    };
+    function _checkWin(currentPlayer){
         let playerMoves = currentPlayer.getMoves();
         let winCheck = (arr, target) => target.every(m =>arr.includes(m));
         wins.forEach(win => {
-            //console.log(win);
-            //console.log(playerMoves);
             if (winCheck(playerMoves,win) == true){ 
-                gameBoard.addPlay(index, currentPlayer.name);
-            }
+                alert(`${currentPlayer.name} won! you suck ${_nextPlayer.name}!`)
+                playing = false;
+                gameBoard.closeOptions();
+            };
         });
-    }
+    };
+    function reset(){
+        playing = true;
+        player1.reset();
+        player2.reset();
+        counter = 0;
+    };
 
     return {
-        addNewPlay : addNewPlay,
+        addNewPlay,
+        reset,
     }
 })();
 
-var player = (function (name, playerVal) {
-    let moves = [];
-    let score = 0;
 
-    function addPlay (){
-        moves.push(1)
-        moves.push(9)
-        moves.push(5)
-    }
-    function getMoves(){
-        return moves; 
-    }
 
-    function addNewMove(newMove){
-        moves.push(newMove);
-        console.log(moves);
-    }
- 
-    return {
-        getMoves : getMoves,
-        addNewMove : addNewMove,
-        playerVal : playerVal,
-        name : name,
-
-    }
-});
-gameBoard.render();
-let player1 = player('Remy', 'X');
-let player2 = player('Lily', 'O');
-console.log(player1.playerVal)
-console.log(player2.playerVal)
